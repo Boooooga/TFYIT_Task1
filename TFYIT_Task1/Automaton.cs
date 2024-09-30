49c6b699407a234d25e505a4a9702905912fbb4e
Егор Бугаев <booooogets@yandex.ru>
2024-09-30 12:29:41 +0400

    Добавлена поддержка перевода НКА в ДКА

--- TFYIT_Task1/Automaton.cs
+++ TFYIT_Task1/Automaton.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +11,12 @@ namespace TFYIT_Task1
     internal class Automaton
     {
         uint type;
-        private string[] states;
-        private string[] inputs;
-        private string[] finalStates;
-        private string initState;
-        private Dictionary<string, List<string>> transitions;
-        private Dictionary<string, int> inpIndexes;
+        private string[]? states;
+        private string[]? inputs;
+        private string[]? finalStates;
+        private string? initState;
+        private Dictionary<string, List<string>>? transitions;
+        private Dictionary<string, int>? inpIndexes;
         bool isInitiatedCorrectly;
 
         public Automaton(string path)
@@ -140,6 +142,7 @@ namespace TFYIT_Task1
             this.finalStates = finalStates;
             this.initState = initState;
             this.transitions = transitions;
+            isInitiatedCorrectly = true;
         }
         public void ShowInfo()
         {
@@ -207,7 +210,7 @@ namespace TFYIT_Task1
                     {
                         for (int j = 0; j < inputs.Length + 1; j++)
                         {
-                            if (j == 0) Console.Write("\t");
+                            if (j == 0) Console.Write($"{{0, {maxLength - 1}}}\t", "");
                             else Console.Write($"|{{0, {-maxLength - 1}}}|", inputs[j - 1]);
                         }
                     }
@@ -219,13 +222,13 @@ namespace TFYIT_Task1
                             {
                                 if (states[i - 1] == initState)
                                 {
-                                    Console.Write($"->{states[i - 1]}:\t");
+                                    Console.Write($"->{{0, {-maxLength - 1}}}:\t", states[i - 1]);
                                 }
                                 else if (finalStates.Contains(states[i - 1]))
                                 {
-                                    Console.Write($" *{states[i - 1]}:\t");
+                                    Console.Write($" *{{0, {-maxLength - 1}}}:\t", states[i - 1]);
                                 }
-                                else Console.Write($"  {states[i - 1]}:\t");
+                                else Console.Write($"  {{0, {-maxLength - 1}}}:\t", states[i - 1]);
                             }
                             else Console.Write($"|{{0, {-maxLength - 1}}}|", transitions[states[i - 1]][j - 1]);
                         }
@@ -282,8 +285,8 @@ namespace TFYIT_Task1
                     else
                         Console.Write($"{i}-'{inputs[i - 1]}'.\n");
                 }
-                Console.WriteLine($"\nТекущее состояние: {currentState}");
                 Console.ResetColor();
+                Console.WriteLine($"\nТекущее состояние: {currentState}");
 
                 foreach (char symbol in word)
                 {
@@ -443,6 +446,257 @@ namespace TFYIT_Task1
                 Console.ResetColor();
                 return false;
             }
+        }
+        private bool ProcessKNA_E(string word)
+        {
+            bool isOk = false;
+            if (isInitiatedCorrectly)
+            {
+                bool EmergencyBreak = false;
+                List<string> inputsList = inputs.ToList();
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("Состояния имеют следующий порядок: ");
+                for (int i = 1; i <= states.Length; i++)
+                {
+                    if (i != states.Length)
+                        Console.Write($"{i}-'{states[i - 1]}', ");
+                    else
+                        Console.Write($"{i}-'{states[i - 1]}'.\n");
+                }
+                Console.Write("Входные символы имеют следующий порядок: ");
+                for (int i = 1; i <= inputs.Length; i++)
+                {
+                    if (i != inputs.Length)
+                        Console.Write($"{i}-'{inputs[i - 1]}', ");
+                    else
+                        Console.Write($"{i}-'{inputs[i - 1]}'.\n");
+                }
+                Console.ResetColor();
+
+                List<string> reachableStates = new List<string>();
+                List<string> currentStates = [initState];
+
+                Console.WriteLine($"\nТекущее состояние: {initState}");
+
+                foreach (char symbol in word)
+                {
+                    if (inputs.Contains(symbol.ToString()) || symbol == 'E') // если символ входит в алфавит
+                                                                            // или является эпсилон-переходом "E"
+                    {
+                        Console.WriteLine($"Считан символ '{symbol}'");
+                        //рассматриваем все состояния, в которых мы находимся на текущем шаге
+                        foreach (string item in currentStates)
+                        {
+                            string tempState = transitions[item][inputsList.IndexOf(symbol.ToString())];
+                            if (tempState.Contains("{")) // если мы переходим не в одно состояние
+                            {
+                                tempState = tempState.Trim(['{', '}']);
+                                foreach (string state in tempState.Split(',')) // сохраняем все достижимые состояния
+                                {
+                                    if (!reachableStates.Contains(state))
+                                        reachableStates.Add(state);
+                                }
+                            }
+                            else // если мы переходим в одно состояние
+                            {
+                                if (!reachableStates.Contains(tempState))
+                                    reachableStates.Add(tempState); // сохраняем это достижимое состояние
+                            }
+                        }
+
+
+                        currentStates = new List<string>(reachableStates);
+                        reachableStates.Clear();
+                        Console.Write(" - Текущее(ие) состояние(ия): ");
+                        foreach (string item in currentStates)
+                        {
+                            Console.Write($"'{item}', ");
+                        }
+                        Console.WriteLine();
+                    }
+                    // если символа нет в алфавите
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Ошибка! Считанный символ '{symbol}' не входит в алфавит!");
+                        Console.ResetColor();
+                        EmergencyBreak = true;
+                        break;
+                    }
+                }
+                if (!EmergencyBreak)
+                {
+                    // если хотя бы одно из текущих состояний входит в число конечных
+                    if (finalStates.ToList().Any(x => currentStates.Any(y => y == x)))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Одно из состояний ");
+                        foreach (string item in currentStates)
+                        {
+                            Console.Write($"'{item}', ");
+                        }
+                        Console.WriteLine("входит в число финальных состояний.");
+                        Console.ResetColor();
+                        isOk = true;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("Ни одно из состояний ");
+                        foreach (string item in currentStates)
+                        {
+                            Console.Write($"'{item}', ");
+                        }
+                        Console.WriteLine("не входит в число финальных состояний.");
+                        Console.ResetColor();
+                    }
+                }
+                return isOk;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Операция 'ProcessInputLine' не может быть выполнена: автомат не проинициализирован.");
+                Console.ResetColor();
+                return false;
+            }
+        }
+        public Automaton KNAtoKDA()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Выполняется преобразование НКА к ДКА:\n");
+            Console.ResetColor();
+
+            List<string> newStates = new List<string>(); // список новых состояний
+            List<string> newFinalStates = new List<string>(); // список новых финальных состояний
+            Dictionary<string, List<string>> newTransitions = new Dictionary<string, List<string>>();
+            bool run = true;
+
+            newStates.Add(initState);
+            string currentState = initState;
+
+            int i = 0;
+            while (run)
+            {
+                if (!newStates[i].Contains("{")) // если текущее состояние однозначно
+                {
+                    foreach (string item in transitions[currentState]) // рассматриваем все смежные состояния
+                    {
+                        if (!newStates.Contains(item)) // и если следующего состояния нет в новом списке
+                        {
+                            newStates.Add(item); // то добавляем его
+
+                            // проверяем, входит ли полученное состояние в число финальных
+                            string[] tempStates = item.Trim(['{', '}']).Split(',');
+                            bool isFinal = false;
+                            foreach (string state in tempStates)
+                            {
+                                if (finalStates.Contains(state))
+                                {
+                                    isFinal = true;
+                                    break;
+                                }
+                            }
+                            if (isFinal)
+                                newFinalStates.Add(item);
+                        }
+                    }
+                    newTransitions.Add(currentState, transitions[currentState]);
+                }
+                else // если текущее состояние не однозначно
+                {
+                    List<string> allReachableStates = new List<string>(); // временный список для всех состояний,
+                                                                         // достижимых из текущго неоднозначного
+
+                    string tempStates = currentState.Trim(['{', '}']); // разделяем текущее состояние на однозначные
+                    List<HashSet<string>> reachableFromInputs = new List<HashSet<string>>(inputs.Length);
+                    for (int j = 0; j < inputs.Length; j++) // инициализация хешсетов
+                    {
+                        reachableFromInputs.Add(new HashSet<string>());
+                    }
+
+
+                    foreach (string state in tempStates.Split(','))   // и рассматриваем по одному
+                    {
+                        for (int j = 0; j < inputs.Length; j++)
+                        {
+                            string[] splitted = transitions[state][j].Trim(['{', '}']).Split(",");
+                            foreach (string symbol in splitted)
+                            {
+                                reachableFromInputs[j].Add(symbol);
+                            }
+                        }
+                    }
+
+
+                    List<string> results = new List<string>(); // вспомогат. список, в котором будут все
+                                                              // результирующие состояния для этого шага
+                    // форматируем достижимые состояния для их добавления в список новых
+                    foreach (var item in reachableFromInputs)
+                    {
+                        string[] statesArray = item.ToArray();
+                        Array.Sort(statesArray);
+                        bool isFinal = false;
+
+                        if (statesArray.Length > 1) // если получили более 1 состояния (т.е. {...})
+                        {
+                            string result = "{";
+                            foreach (string elem in statesArray)
+                            {
+                                result += elem + ",";
+                                if (finalStates.Contains(elem))
+                                    isFinal = true;
+                            }
+
+                            result = result.TrimEnd(',') + "}";
+
+                            if (isFinal)
+                                newFinalStates.Add(result);
+                            results.Add(result);
+
+                            if (!newStates.Contains(result)) // если такое состояние ещё не встречалось
+                            {
+                                newStates.Add(result);
+                            }
+                        }
+                        else // если получили одно состояние
+                        {
+                            if (finalStates.Contains(statesArray[0]))
+                                newFinalStates.Add(statesArray[0]);
+                            results.Add(statesArray[0]);
+
+                            if (!newStates.Contains(statesArray[0]))
+                            {
+                                newStates.Add(statesArray[0]);
+                            }
+                        }
+                    }
+                    newTransitions.Add("{" + tempStates + "}", new List<string>(results));
+                    results.Clear();
+                }
+
+
+                i++;
+                if (i >= newStates.Count)
+                {
+                    run = false;
+                    break;
+                }
+                currentState = newStates[i];
+            }
+
+            //foreach (var item in newTransitions)
+            //{
+            //    Console.Write($"{item.Key}: ");
+            //    foreach (string elem in item.Value)
+            //    {
+            //        Console.Write($"{elem} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            return new Automaton(1, newStates.ToArray(), inputs, newFinalStates.ToArray(), initState, newTransitions);
         }
     }
 }
--- TFYIT_Task1/Program.cs
+++ TFYIT_Task1/Program.cs
@@ -9,15 +9,19 @@ namespace TFYIT_Task1
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\123\Desktop\KNA1.txt";
+            string path = @"C:\Users\123\Desktop\NKA.txt";
             Automaton myAuto = new Automaton(path);
             myAuto.ShowInfo();
             Console.WriteLine();
-            myAuto.ShowTable();
-            string inLine;
-            Console.Write("\nВведите входное слово: ");
-            inLine = Console.ReadLine();
-            myAuto.ProcessInputLine(inLine);
+            //myAuto.ShowTable();
+            //string inLine;
+            //Console.Write("\nВведите входное слово: ");
+            //inLine = Console.ReadLine();
+            //myAuto.ProcessInputLine(inLine);
+
+            Automaton newAuto = myAuto.KNAtoKDA();
+            newAuto.ShowInfo();
+            newAuto.ShowTable();
         }
     }
 }
